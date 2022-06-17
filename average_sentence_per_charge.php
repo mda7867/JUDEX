d61bea45d1bd87b03ee5c8c762b0dce1ef44b55c
macbook <macbook@ajibola.bmcc.cuny.edu>
2022-06-16 20:20:55 -0400

    updated navbar links to pages and the null values for table lookup

--- average_sentence_per_charge.php
+++ average_sentence_per_charge.php
@@ -1,6 +1,12 @@
 
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/styles.css">
+  <title>Average Sentences</title>
+</head>
 <body>
 
 <h2>Average Sentence</h2>
